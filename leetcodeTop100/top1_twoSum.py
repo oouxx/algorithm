cd0739b9d7903dda0122eb60c50a502d35a37ed9
unknown <oouxx>
2019-12-06 20:38:54 +0800

    delete ccf_csp

--- leetcodeTop100/top1_twoSum.py
+++
@@ -1,9 +0,0 @@
-# 保存哈希表法
-class Solution(object):
-    def twoSum(self, nums, target):
-        nums_dict = dict()
-        for index in range(len(nums)):
-            temp = target - nums[index]
-            if temp in nums_dict and nums_dict[temp] != index:
-                return [index, nums_dict[temp]]
-            nums_dict[nums[index]] = index
